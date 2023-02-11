224c919355b9764c8dc78a00291bcea6546dc981
Jessica Ferreira <jessiberteloni@gmail.com>
2023-02-11 15:14:34 -0300

    Primeiros passos com Ruby

--- cookbook.rb
+++ cookbook.rb
@@ -5,10 +5,13 @@ receitas = []
 while(true)do
     puts "Digite o nome da receita: "
     nome = gets.chomp()
+
     receitas << nome
+
     puts
     puts "Receita #{nome} cadastrada com sucesso!"
     puts
+    puts "========== Receitas cadastradas =========="
     puts receitas
     puts
 end
