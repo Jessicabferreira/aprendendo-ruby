aba2bf79d8e15209f5425601df05d73766220a7a
Jessica Ferreira <jessiberteloni@gmail.com>
2023-02-14 17:17:26 -0300

    Primeiros passos com Ruby

--- cookbook.rb
+++ cookbook.rb
@@ -9,18 +9,28 @@ puts "[3] Sair"
 print "Escolha uma opçaõ: "
 opcao = gets.to_i()
 
-while(opcao != 3)do
+while(opcao != 3) do
+  if(opcao == 1)
     puts "Digite o nome da receita: "
     nome = gets.chomp()
-
     receitas << nome
-
     puts
     puts "Receita #{nome} cadastrada com sucesso!"
     puts
+  elsif(opcao == 2)
     puts "========== Receitas cadastradas =========="
     puts receitas
     puts
+  else
+    puts "Opção inválida"
 end
+
+puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+
+print "Escolha uma opção: "
+opcao = gets.to_i()
+end 
 
 puts "|Obrigado por usar o Cookbook, até logo"
