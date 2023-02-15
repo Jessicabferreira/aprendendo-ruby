802f5ecd5d12c6b11aed5dd741f9dd7e581a0736
Jessica Ferreira <jessiberteloni@gmail.com>
2023-02-15 18:59:15 -0300

    Praticando Ruby

--- conta.rb
+++ conta.rb
@@ -1,3 +1,17 @@
 class Conta
-    
-end
+    attr_reader :numero, :titular
+    attr_accessor :saldo
+
+    def initialize(numero, titular, saldo)
+        @numero = numero
+        @titular = titular
+        @saldo = saldo
+    end
+
+    def sacar(valor)
+        if saldo >= valor
+            self.saldo -= valor
+        else
+            puts "Não foi possivel executar o saque"
+        end
+    end
