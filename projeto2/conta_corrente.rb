79ffe5e077faee31dc3d3f0c8c061bc0a8560d13
Jessica Ferreira <jessiberteloni@gmail.com>
2023-02-16 17:50:43 -0300

    Praticando Ruby

---
+++ projeto2/conta_corrente.rb
@@ -0,0 +1,17 @@
+class ContaCorrente
+    def initialize(numero, titular, saldo, limite)
+        @numero = numero
+        @titular = titular
+        @saldo = saldo
+        @limite = limite
+    end
+
+    def sacar
+    end
+
+    def depositar
+    end
+
+    def transferir
+    end
+end
